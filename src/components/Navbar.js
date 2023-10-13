5a02d3781d25dab13d914b23b5bebe44554b0041
Ranahansa <CODSE213F-053@student.nibm.lk>
2023-10-13 15:32:58 +0530

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function  Navbar() {
     return (
         <div className="navbar">
             <img id ="logo" className="logo" src="https://images.pexels.com/photos/12910529/pexels-photo-12910529.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" />
-            <h1 className="title">Select Your Order</h1>
+            <h1 className="title">Select Your</h1>
         </div>
     )
 }
